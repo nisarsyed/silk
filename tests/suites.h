6fb5897842e5a690b68a750e95f13ecd77db1cdc
Nisar Syed <nisarsyed510@gmail.com>
2026-08-24 18:42:39 +0500

    feat: project scaffold (#1)
    * feat: cmake build system with core library targets
    
    * feat: lightweight test framework with ctest integration
    
    * feat: sandbox example stub linked against core

---
+++ tests/suites.h
@@ -0,0 +1,8 @@
+/* Each suite exposes a runner returning its failure count.
+ * Add a declaration here when adding a new suite file. */
+#ifndef SILK_SUITES_H
+#define SILK_SUITES_H
+
+int sl_core_suite(void);
+
+#endif /* SILK_SUITES_H */
